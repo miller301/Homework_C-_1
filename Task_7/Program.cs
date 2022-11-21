016b2bd51c794c2a6f07d4c85d6747aa922deb4e
miller301 <perepelkin2241@yandex.ru>
2022-11-21 13:32:47 +0300

    task 7 is done

---
+++ Task_7/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int d = 0, k = 0;
+if (number > 99)
+{
+    k = number;
+    while (k >= 1000)
+    {
+        k /= 10;
+        d = k % 10;
+
+    }
+Console.WriteLine($"Третье число от начала {d}");
+}
+else
+{
+    Console.WriteLine("Число имеет меньше трех цифр");
+}
